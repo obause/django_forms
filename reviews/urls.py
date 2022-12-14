ce1aea0f104f385a22c4a9256837598170864d1c
Ole Bause <ole@bause.me>
2022-12-14 16:51:08 +0100

    added more views and templates

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.review),
-    path("submit-success", views.ReviewView.as_view())
+    path("", views.ReviewView.as_view()),
+    path("submit-success", views.SubmitSuccessView.as_view()),
+    path("reviews", views.ReviewsListView.as_view()),
+    path("reviews/<int:pk>", views.ReviewDetailView.as_view())
 ]
